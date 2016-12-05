bde1dc74a94c5c849b27135849d50a1d63cfd92a
joellegg <joellegg@bellsouth.net>
2016-12-05 13:17:13 -0600

    removed product card, added loop to log items and price to console

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,13 +1,26 @@
+// product, department, price
 function getSeason () {
     var season = document.getElementById("season");
     season = season.options[season.selectedIndex].text;
     console.log(season)
 }
 
+function logToConsole(e) {
+    var productData = JSON.parse(e.target.responseText);
+    for (var i = 0; i < productData.products.length; i++) {
+        console.log(productData.products[i].name);
+        console.log(productData.products[i].price);
+    }
+}
+
+// event listener on the season select option then run getSeason function
 var selectSeason = document.getElementById("season");
 selectSeason.addEventListener("change", getSeason);
 
+// get products from JSON file and run logToConsole function
 var productData = new XMLHttpRequest();
-productData.addEventListener('load', executeSomeCodeAfterLoad);
-productData.open("GET", "product.json");
-myRequest.send();
+productData.addEventListener('load', logToConsole);
+productData.open("GET", "products.json");
+productData.send();
+
+// get reference to div to write products
