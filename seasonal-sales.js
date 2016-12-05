b3e20bb7d9aac6d67290be9739500a702cbe5024
joellegg <joellegg@bellsouth.net>
2016-12-05 15:55:13 -0600

    nested functions to utilize both json files and multiply variables

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,46 +1,80 @@
 // product (products name), department (products categories_id === categories id), price (products price + categories discount)
-function getSeason () {
-    var season = document.getElementById("season");
+var productData;
+var productCategories;
+var season;
+// reference to HTML products and categories
+var categoriesHTML = document.getElementById('categories');
+var productsHTML = document.getElementById('products');
+
+function getSeason() {
+    season = document.getElementById("season");
     season = season.options[season.selectedIndex].text;
     console.log(season)
 }
+// run productsToHTML function
+    // run categoriesToParse function passing productData
+        // run discountFunction passing productData and productCategories
+        // write to HTML the discounted price and misc data :)
 
-function productsToConsole(e) {
-    var productData = JSON.parse(e.target.responseText);
-    var productsHTML = document.getElementById('products');
+// 3.
+function productsToParse(e) {
+    productData = JSON.parse(e.target.responseText);
+    loadJSON(productCategories, categoriesToParse, 'categories.json');
+}
+
+// 5.
+function categoriesToParse(e) {
+    productCategories = JSON.parse(e.target.responseText);
+    writeToHTML();
+}
+
+// 6.
+function writeToHTML() {
+    // change category id to category name
     for (var i = 0; i < productData.products.length; i++) {
+        var department = productData.products[i].category_id
+        if (department === 1) {
+            department = 'Winter';
+        } else if (department === 2) {
+            department = 'Autumn';
+        } else if (department === 3) {
+            department === 'Spring';
+        } else {
+            alert('missing department for ' + productData.products[i].name)
+        }
         var newDiv = document.createElement('div');
         newDiv.innerHTML = `
-            <h2>The id is ${productData.products[i].id}</h2>
-            <h4>The name is ${productData.products[i].name}</h4>
-            <h4>The price is ${productData.products[i].price}</h4>
-            <h4>The category id is ${productData.products[i].category_id}</h4>
+            <h2>Item: ${productData.products[i].name}</h2>
+            <h4>Price: ${productData.products[i].price}</h4>
+            <h4>Department: ${department}</h4>
         `;
         productsHTML.appendChild(newDiv);
     }
-}
-
-function categoriesToConsole(e) {
-    var productCategories = JSON.parse(e.target.responseText);
     for (var i = 0; i < productCategories.categories.length; i++) {
-        console.log('id is ' + productCategories.categories[i].id);
-        console.log('name is ' + productCategories.categories[i].name);
-        console.log('season is ' + productCategories.categories[i].season);
-        console.log('discount is ' + productCategories.categories[i].discount);
+    var newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+        <h2>The id is ${productCategories.categories[i].id}</h2>
+        <h4>The name is ${productCategories.categories[i].name}</h4>
+        <h4>The season is ${productCategories.categories[i].season_discount}</h4>
+        <h4>The discount is ${productCategories.categories[i].discount}</h4>
+    `;
+    categoriesHTML.appendChild(newDiv);
     }
+    var discountedPrice = productData.products[0].price * (1 - productCategories.categories[0].discount);
+    console.log(discountedPrice.toFixed(2));
+    console.log('4.99 * (1-0.1) = 4.49');
 }
 
-// get products from JSON file and run logToConsole function
-var productData = new XMLHttpRequest();
-productData.addEventListener('load', productsToConsole);
-productData.open("GET", "products.json");
-productData.send();
+// 2., 4.
+function loadJSON (varData, functionToRun, jsonFile) {
+    var varData = new XMLHttpRequest();
+    varData.addEventListener('load', functionToRun);
+    varData.open("GET", jsonFile);
+    varData.send();
+}
 
-// get categories from JSON file and run logToConsole function
-var productCategories = new XMLHttpRequest();
-productCategories.addEventListener('load', categoriesToConsole);
-productCategories.open("GET", "categories.json");
-productCategories.send();
+// 1. This function will use the loadJSON function to load the json file and then run function productsToParse
+loadJSON(productData, productsToParse, 'products.json');
 
 // event listener on the season select option then run getSeason function
 var selectSeason = document.getElementById("season");
