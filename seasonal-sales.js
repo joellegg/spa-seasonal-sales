de96e3703bbdb285ec4323cd79a4d2b0e0008dbe
joellegg <joellegg@bellsouth.net>
2016-12-06 15:53:59 -0600

    rearranged discountfunction for loop to run only on items in correct season discount

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,21 +1,14 @@
 // product (products name), department (products categories_id === categories id then category name), price (products price + categories discount)
 var productData;
 var productCategories;
-var season;
+var currentSeason;
 // reference to HTML products and categories
 var categoriesHTML = document.getElementById('categories');
 var productsHTML = document.getElementById('products');
 var productDepartment;
 
-function getSeason() {
-    season = document.getElementById("season");
-    season = season.options[season.selectedIndex].text;
-    discountedPrice();
-}
-// run productsToHTML function
-    // run categoriesToParse function passing productData
-        // run discountFunction passing productData and productCategories
-        // write to HTML the discounted price and misc data :)
+// 1. This function will use the loadJSON function to load the json file and then run function productsToParse
+loadJSON(productData, productsToParse, 'products.json');
 
 // 2., 4.
 function loadJSON (varData, functionToRun, jsonFile) {
@@ -35,52 +28,55 @@ function productsToParse(e) {
 function categoriesToParse(e) {
     productCategories = JSON.parse(e.target.responseText);
     getSeason();
-    // try taking this function out to see if it will work without
-    // discountedPrice();
 }
 
 // 6.
-function discountedPrice() {
-    if (season === productCategories.categories[0].season_discount) {
-        for (var i = 0; i < productData.products.length; i++) {
-            productData.products[i].price *= (1 - productCategories.categories[0].discount);
-            console.log("the season is Winter", productData.products[i].price.toFixed(2));
-        }
-    } else if (season === productCategories.categories[1].season_discount) {
-        for (var i = 0; i < productData.products.length; i++) {
-            productData.products[i].price *= (1 - productCategories.categories[1].discount);
-            console.log("the season is Autumn", productData.products[i].price.toFixed(2));
-        }
-    } else if (season === productCategories.categories[2].season_discount) {
-        for (var i = 0; i < productData.products.length; i++) {
-            productData.products[i].price *= (1 - productCategories.categories[2].discount);
-            console.log("the season is Spring", productData.products[i].price.toFixed(2));
-        }
-    }
+function getSeason() {
+    currentSeason = document.getElementById("season");
+    currentSeason = currentSeason.options[currentSeason.selectedIndex].text;
+    getCategory();
+}
+
+// 7.
+function getCategory() {
     // change category id to category name (ie category_id 1 = apparel)
     for (var i = 0; i < productData.products.length; i++) {
         if (productData.products[i].category_id === productCategories.categories[0].id) {
-            productDepartment = productCategories.categories[0].name;
+            productData.products[i].category_id = productCategories.categories[0].name;
         } else if (productData.products[i].category_id === productCategories.categories[1].id) {
-            productDepartment = productCategories.categories[1].name;
+            productData.products[i].category_id = productCategories.categories[1].name;
         } else if (productData.products[i].category_id === productCategories.categories[2].id) {
-            productDepartment = productCategories.categories[2].name;
-        } else {
-            alert('missing department for ' + productData.products[i].name)
+            productData.products[i].category_id = productCategories.categories[2].name;
+        }
     }
-    var newDiv = document.createElement('div');
-    newDiv.innerHTML = `
-        <h2>Item: ${productData.products[i].name}</h2>
-        <h4>Price: ${productData.products[i].price}</h4>
-        <h4>Department: ${productDepartment}</h4>
-    `;
-    productsHTML.appendChild(newDiv);
-    }
+    discountedPrice();
 }
 
+// 8.
+function discountedPrice() {
+    for (var i = 0; i < productData.products.length; i++) {
+        if (currentSeason === productCategories.categories[0].season_discount && productData.products[i].category_id === productCategories.categories[0].name) {
+            productData.products[i].price *= (1 - productCategories.categories[0].discount);
+        } else if (currentSeason === productCategories.categories[1].season_discount && productData.products[i].category_id === productCategories.categories[1].name) {
+            productData.products[i].price *= (1 - productCategories.categories[1].discount);
+        } else if (currentSeason === productCategories.categories[2].season_discount && productData.products[i].category_id === productCategories.categories[2].name) {
+            productData.products[i].price *= (1 - productCategories.categories[2].discount);
+        }
+    }
+    writeToHTML();
+}
 
-// 1. This function will use the loadJSON function to load the json file and then run function productsToParse
-loadJSON(productData, productsToParse, 'products.json');
+// 9.
+function writeToHTML() {
+    productsHTML.innerHTML = "";
+    for (var i = 0; i < productData.products.length; i++) {
+        productsHTML.innerHTML += `
+            <h2>Item: ${productData.products[i].name}</h2>
+            <h4>Price: ${productData.products[i].price.toFixed(2)}</h4>
+            <h4>Department: ${productData.products[i].category_id}</h4>
+        `;
+    }
+}
 
 // event listener on the season select option then run getSeason function
 var selectSeason = document.getElementById("season");
