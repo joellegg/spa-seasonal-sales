f8614df1cc4a73b67f547a0754b81fc705567de7
joellegg <joellegg@bellsouth.net>
2016-12-07 08:59:25 -0600

    added discount season to products and show discount price if current season is discount season

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -7,21 +7,25 @@ var categoriesHTML = document.getElementById('categories');
 var productsHTML = document.getElementById('products');
 var productDepartment;
 
+// event listener on the season select option then run getSeason function
+var selectSeason = document.getElementById("season");
+selectSeason.addEventListener("change", getSeason);
+
 // 1. This function will use the loadJSON function to load the json file and then run function productsToParse
-loadJSON(productData, productsToParse, 'products.json');
+loadJSON(productsToParse, 'products.json');
 
 // 2., 4.
-function loadJSON (varData, functionToRun, jsonFile) {
-    var varData = new XMLHttpRequest();
-    varData.addEventListener('load', functionToRun);
-    varData.open("GET", jsonFile);
-    varData.send();
+function loadJSON (functionToRun, jsonFile) {
+    var newRequest = new XMLHttpRequest();
+    newRequest.addEventListener('load', functionToRun);
+    newRequest.open("GET", jsonFile);
+    newRequest.send();
 }
 
 // 3.
 function productsToParse(e) {
     productData = JSON.parse(e.target.responseText);
-    loadJSON(productCategories, categoriesToParse, 'categories.json');
+    loadJSON(categoriesToParse, 'categories.json');
 }
 
 // 5.
@@ -32,8 +36,9 @@ function categoriesToParse(e) {
 
 // 6.
 function getSeason() {
-    currentSeason = document.getElementById("season");
-    currentSeason = currentSeason.options[currentSeason.selectedIndex].text;
+    var seasonElement = document.getElementById("season");
+    currentSeason = seasonElement.options[seasonElement.selectedIndex].text;
+    // console.log(currentSeason);
     getCategory();
 }
 
@@ -41,12 +46,12 @@ function getSeason() {
 function getCategory() {
     // change category id to category name (ie category_id 1 = apparel)
     for (var i = 0; i < productData.products.length; i++) {
-        if (productData.products[i].category_id === productCategories.categories[0].id) {
-            productData.products[i].category_id = productCategories.categories[0].name;
-        } else if (productData.products[i].category_id === productCategories.categories[1].id) {
-            productData.products[i].category_id = productCategories.categories[1].name;
-        } else if (productData.products[i].category_id === productCategories.categories[2].id) {
-            productData.products[i].category_id = productCategories.categories[2].name;
+        for (var k = 0; k < productCategories.categories.length; k++)
+            if (productData.products[i].category_id === productCategories.categories[k].id) {
+                productData.products[i].category_name = productCategories.categories[k].name;
+                productData.products[i].discounted_season = productCategories.categories[k].season_discount;
+                console.log("product dis, cat dis", productData.products[i].discounted_season, productCategories.categories[k].season_discount);
+                //console.log("product-category", productData.products[i].category_name, productCategories.categories[k].name);
         }
     }
     discountedPrice();
@@ -55,29 +60,36 @@ function getCategory() {
 // 8.
 function discountedPrice() {
     for (var i = 0; i < productData.products.length; i++) {
-        if (currentSeason === productCategories.categories[0].season_discount && productData.products[i].category_id === productCategories.categories[0].name) {
-            productData.products[i].price *= (1 - productCategories.categories[0].discount);
-        } else if (currentSeason === productCategories.categories[1].season_discount && productData.products[i].category_id === productCategories.categories[1].name) {
-            productData.products[i].price *= (1 - productCategories.categories[1].discount);
-        } else if (currentSeason === productCategories.categories[2].season_discount && productData.products[i].category_id === productCategories.categories[2].name) {
-            productData.products[i].price *= (1 - productCategories.categories[2].discount);
+        for (var k = 0; k < productCategories.categories.length; k++) {
+            if (currentSeason === productCategories.categories[k].season_discount && productData.products[i].category_name === productCategories.categories[k].name) {
+                productData.products[i].discounted_price = (productData.products[i].price * (1 - productCategories.categories[k].discount));
+                console.log('season discount', currentSeason, productCategories.categories[k].season_discount, productData.products[i].category_name, productCategories.categories[k].name);
+            }
         }
     }
+    console.log(productData.products);
     writeToHTML();
 }
 
-// 9.
+// 9. if statement to determine prices to post
 function writeToHTML() {
     productsHTML.innerHTML = "";
     for (var i = 0; i < productData.products.length; i++) {
-        productsHTML.innerHTML += `
-            <h2>Item: ${productData.products[i].name}</h2>
-            <h4>Price: ${productData.products[i].price.toFixed(2)}</h4>
-            <h4>Department: ${productData.products[i].category_id}</h4>
-        `;
+        // if current season is the discount season then show discounted price
+        if (currentSeason === productData.products[i].discounted_season) {
+            productsHTML.innerHTML += `
+                <h2>Item: ${productData.products[i].name}</h2>
+                <h4>Price: ${productData.products[i].discounted_price.toFixed(2)}</h4>
+                <h4>Department: ${productData.products[i].category_name}</h4>
+            `;
+        }
+        // else show the full price
+        else {
+            productsHTML.innerHTML += `
+                <h2>Item: ${productData.products[i].name}</h2>
+                <h4>Price: ${productData.products[i].price.toFixed(2)}</h4>
+                <h4>Department: ${productData.products[i].category_name}</h4>
+            `;
+        }
     }
 }
-
-// event listener on the season select option then run getSeason function
-var selectSeason = document.getElementById("season");
-selectSeason.addEventListener("change", getSeason);
